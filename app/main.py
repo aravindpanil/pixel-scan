06cd8465e6c80cd407ad09098541e0c67cdae33f
Aravind P Anil <aravindpanil@gmail.com>
2025-06-30 18:57:01 +0530

    Added error handling

--- app/main.py
+++ app/main.py
@@ -5,16 +5,33 @@ from pathlib import Path
 
 # Configuration variables
 samples_dir = Path("app/samples")
-image_filename = "Coffee.jpg"
-image_path = samples_dir / image_filename
-image = Image.open(image_path)
+image_filename = "Glas.jpg"
 
 # Get EXIF data 
-exifdata = image.getexif()
-readable_exif = {}
-for tag_id, value in exifdata.items():
-    tag = TAGS.get(tag_id, tag_id)
-    readable_exif[tag] = value
+def get_exif_data(image):
+    exifdata = image.getexif()
+    readable_exif = {}
+    for tag_id, value in exifdata.items():
+        tag = TAGS.get(tag_id, tag_id)
+        readable_exif[tag] = value
+    
+    return readable_exif
 
-# Print some metadata
-print(readable_exif)
+# Open file with Error handling
+try:
+    image_path = Path(samples_dir/image_filename)
+    image = Image.open(image_path)
+    
+    readable_exif = get_exif_data(image)
+        # Print EXIF data
+    if readable_exif:
+        print("\nEXIF Data:\n")
+        for tag, value in readable_exif.items():
+                print(f"{tag}: {value}")
+    else:
+        print("No EXIF data found")
+
+except FileNotFoundError as e:
+    print(f"File not found: {e}. Please check the image name")
+except Exception as e:
+    print(f"Error opening image: {e}")
