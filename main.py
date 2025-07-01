d1180b62e1e818cb7e26e0038946d4f284c17507
Aravind P Anil <aravindpanil@gmail.com>
2025-07-01 10:47:09 +0530

    Added function to show filename

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ from flask import Flask, request, render_template
 # Import utils which contains metadata extractor method
 from app import utils
 
-# Define flask main configuration app container
+# Define flask application instance
 app = Flask(__name__)
 
 # Define default root route with POST method as default is only GET
 @app.route('/', methods=['GET', 'POST'])
 def index():
     metadata = {}
+    filename = None
+    
     if request.method == 'POST':
         if 'image' not in request.files:
             metadata['Error'] = 'No file found'
-        file = request.files['image']
-        if file.filename != '':
+        
+        file = request.files.get("image")
+        if file and file.filename != "":
+            filename = file.filename
             metadata = utils.get_exif_data(file.stream)
-    
-    # Use template
-    return render_template('index.html', metadata=metadata)
+
+    # Use Template
+    return render_template("index.html", metadata=metadata, filename=filename)
